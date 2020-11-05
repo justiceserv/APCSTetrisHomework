f71096e44d29c551d10eaec2e8cb05a208986657
Justiceserv <justiceserv@jserv.xyz>
2020-11-05 09:49:04 +0900

    11/5 Final?

--- src/tetris/GameThread.java
+++ src/tetris/GameThread.java
@@ -31,6 +31,10 @@ public class GameThread extends Thread
                     Thread.sleep( 300 );
                 } catch (InterruptedException ex) {}
             }
+            //if the block.getY() < 0 => gameOver
+            //1. display a gameover message using a popup window with a textfield.
+            //2. stop the game thread
+            //3. the arrow keys must stop working
             ga.moveBlockToBackground(); 
             score += ga.clearLines(); 
             
@@ -46,7 +50,7 @@ public class GameThread extends Thread
         
         if(newlevel > level)
         {
-            pause -= speedPerLevel; 
+            pause = pause <= 100 ? 100 : pause - speedPerLevel; 
             level = newlevel;
             gf.setLevel(level);
         }
