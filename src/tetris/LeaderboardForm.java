47d45e0607b2612745e3810a8204e9e685022783
Justiceserv <justiceserv@jserv.xyz>
2020-11-09 08:46:50 +0900

    Changed LeaderBoard form 11/7

--- src/tetris/LeaderboardForm.java
+++ src/tetris/LeaderboardForm.java
@@ -34,13 +34,10 @@ public class LeaderboardForm extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Player", "Score"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
